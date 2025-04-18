c4d392b5c097fa4a5f68a4b30579c9774ad10484
Chris Risner <chris@driv.ly>
2025-04-18 09:15:10 -0500

    fix: update prisma binary to work on vercel

--- src/lib/email/reset-password.tsx
+++ src/lib/email/reset-password.tsx
@@ -31,15 +31,15 @@ export const ResetPasswordEmail = ({
 				<Body className="bg-white my-auto mx-auto font-sans px-2">
 					<Container className="border border-solid border-[#eaeaea] rounded my-[40px] mx-auto p-[20px] max-w-[465px]">
 						<Heading className="text-black text-[24px] font-normal text-center p-0 my-[30px] mx-0">
-							Reset your <strong>Better Auth</strong> password
+							Reset your <strong>Better Auth Demo</strong> password
 						</Heading>
 						<Text className="text-black text-[14px] leading-[24px]">
 							Hello {username},
 						</Text>
 						<Text className="text-black text-[14px] leading-[24px]">
 							We received a request to reset your password for your Better Auth
-							account. If you didn't make this request, you can safely ignore
-							this email.
+							Demo account. If you didn't make this request, you can safely
+							ignore this email.
 						</Text>
 						<Section className="text-center mt-[32px] mb-[32px]">
 							<Button
